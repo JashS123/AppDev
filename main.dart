c8de61a3c21ae8e67f1e59ca7e203d304ab323d2
JashS123 <jash.sachdev007@gmail.com>
2025-10-27 00:23:14 +0530

    Add files via upload

--- main.dart
+++ main.dart
@@ -4,80 +4,33 @@ void main() {
   runApp(const MyApp());
 }
 
-
-class MyApp extends StatefulWidget {
+class MyApp extends StatelessWidget{
   const MyApp({super.key});
 
   @override
-  State<MyApp> createState() => _MyAppState();
-}
-
-class _MyAppState extends State<MyApp> {
-  final TextEditingController _controller = TextEditingController();
-
-
-  String displayText = '';
-
-  @override
-  Widget build(BuildContext context) {
+  Widget build(BuildContext context){
     return MaterialApp(
-      title: 'Text Input Example',
-      home: Scaffold(
-        appBar: AppBar(
-          title: const Text('Text Input & Display'),
-          backgroundColor:Colors.deepPurpleAccent
+      title:'FirstApp',
+      home:Scaffold(
+        appBar:AppBar(
+          title:const Text('First App'),
+          backgroundColor:Colors.blue,
         ),
-        body:
-        Padding(
-          padding: const EdgeInsets.all(16.0),
-          child: Column(
-
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: [
-              // TextField where user types text
-              TextField(
-                controller: _controller,
-                decoration: const InputDecoration(
-                  border: OutlineInputBorder(),
-                  labelText: 'Enter something...',
+          body: const Center(
+              child: Text(
+                'Hello World!',
+                style: TextStyle(
+                  fontSize: 24,              // text size
+                  fontWeight: FontWeight.bold, // make it bold
+                  color: Colors.red,
                 ),
               ),
-
-              const SizedBox(height: 20), // adds vertical space
-
-              // Button that updates the text when pressed
-              ElevatedButton(
-                onPressed: () {
-                  setState(() {
-                    // setState tells Flutter to rebuild the UI with updated data
-                    displayText = _controller.text; // store input in displayText
-                  });
-                },
-                style: ElevatedButton.styleFrom(
-                  backgroundColor: Colors.black, // button color
-
-                ),
-                child: const Text(
-                  'Show Text',
-                  style: TextStyle(fontSize: 18, color: Colors.white),
-                ),
-              ),
-
-              const SizedBox(height: 30),
-
-              // Display the text that was entered
-              Text(
-                displayText.isEmpty ? 'Your text will appear here!': displayText,
-                style: const TextStyle(
-                  fontSize: 22,
-                  fontWeight: FontWeight.bold,
-                  color: Colors.deepPurple,
-                ),
-              ),
-            ],
-          ),
-        ),
+          ),// text color
       ),
+
+
     );
   }
+
+
 }
